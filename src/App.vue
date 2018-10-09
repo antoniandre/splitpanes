e1fa9d0ccc5e0dd4438cfe7c643d4a7b97ce27e6
Antoni André <antoniandre.web@gmail.com>
2018-10-09 22:46:04 +1100

    Improve demo layout.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template lang="pug">
   v-app.white
-    v-content
-      img(alt="Vue logo" src="./assets/logo.png" height="40")
-      h1.headline Vue Split Panes
+    v-container
+      div.text-xs-center
+        img(alt="Vue logo" src="./assets/logo.png" height="40")
+        h1.mb-3.headline Vue Split Panes
+        p.subheading A Vue JS reliable and simple panes splitter / resizer.
 
-      p.subheading A Vue JS reliable and simple panes splitter / resizer.
+      h2.mt-5.mb-2.title Features
+      ul
+        li Nesting supported!
+        li Light weight & no dependencies other than Vue JS.
+        li Simple yet efficient.
 
-      p.subheading # Demo 1
-      split-panes.demo(vertical style="height:400px")
+      p.mt-5.subheading # Demo 1
+      split-panes(vertical style="height:400px")
         span 1
         split-panes(horizontal)
           span 2
@@ -15,14 +21,13 @@
           span 4
         span 5
 
-      p.subheading # Demo 2
-      split-panes.demo(horizontal style="height:400px")
-        span 1
+      p.mt-5.subheading # Demo 2
+      split-panes(horizontal style="height:400px")
         split-panes(vertical)
+          span 1
           span 2
           span 3
-          span 4
-        span 5
+        span 4
 </template>
 
 <script>
@@ -39,13 +44,8 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-.demo {
-  margin: 3em;
 }
 
 .split-panes {
@@ -53,6 +53,9 @@ export default {
 
   &__pane {
     box-shadow: 0 0 3px rgba(0, 0, 0, .2) inset;
+    justify-content: center;
+    align-items: center;
+    display: flex;
   }
 
   &__splitter {
@@ -61,5 +64,12 @@ export default {
 
   &--vertical > .split-panes__splitter {width: 10px;}
   &--horizontal > .split-panes__splitter {height: 10px;}
+
+  span {
+    font-family: Helvetica, Arial, sans-serif;
+    color: #fff;
+    font-size: 5em;
+    opacity: 0.6;
+  }
 }
 </style>
