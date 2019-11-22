7e3ba0914d9aa8fe1848334fa5a935e2b08b5eb0
Isaac Vargas <isaac.vargas@mind.pt>
2019-11-22 13:35:34 +0000

    Fixes working in IE, and build-bundle command. Updates dependencies.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,7 @@ module.exports = {
     node: true
   },
   'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
+    'plugin:vue/essential'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,3 @@
 module.exports = {
-  "presets": [
-    [
-      "@vue/cli-plugin-babel/preset",
-      {
-        "useBuiltIns": "entry"
-      }
-    ]
-  ]
+  presets: ['@vue/cli-plugin-babel/preset']
 }
