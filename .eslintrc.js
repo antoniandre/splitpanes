2a196e3dd3a85272dc80a2eef5b1d0f4111921a8
Antoni André <antoniandre.web@gmail.com>
2019-11-24 23:33:10 +1100

    Cleanup eslint and add editorconfig.

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     'plugin:vue/essential'
   ],
   rules: {
+    'brace-style': [2, 'stroustrup', { 'allowSingleLine': true }],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,21 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/splitpanes' : '',
   outputDir: 'docs',
-  lintOnSave: false,
+  lintOnSave: true,
+  devServer: {
+    overlay: {
+      errors: false,
+      warnings: false
+    }
+  },
   chainWebpack: config => {
+    // Preserve white spaces for ssh-pre component.
     config.module
       .rule('vue')
       .use('vue-loader')
       .loader('vue-loader')
       .tap(options => {
-        options.compilerOptions.whitespace = "preserve"
+        options.compilerOptions.whitespace = 'preserve'
         return options
       })
   }
