a009e840292bc792147cc9b11380f91190642c37
Antoni Andre <antoniandre.web@gmail.com>
2020-10-05 16:52:25 +1100

    [#102] Fix looping through HTML collection.

--- src/components/splitpanes/splitpanes.vue
+++ src/components/splitpanes/splitpanes.vue
@@ -304,7 +304,8 @@ export default {
     },
 
     checkSplitpanesNodes () {
-      this.container.children.forEach(child => {
+      const children = Array.from(this.container.children)
+      children.forEach(child => {
         const isPane = child.classList.contains('splitpanes__pane')
         const isSplitter = child.classList.contains('splitpanes__splitter')
 
