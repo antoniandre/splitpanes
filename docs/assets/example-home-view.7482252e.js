05df0f467dd50a7d35799f431375bd529224fea8
antoni andre <antoniandre.web@gmail.com>
2022-02-22 11:58:39 -0500

    Publish documentation on Github.

--- docs/assets/example-home-view.7482252e.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as t,i as s}from"./vendor.ff6ec80c.js";import{_ as o}from"./index.558f647b.js";const c={class:"green-light5--bg w-flex align-center justify-center"},r=s("div",{class:"title1"},"This is home",-1),i=[r];function a(_,l){return e(),t("div",c,i)}const n={};var f=o(n,[["render",a]]);export{f as default};
