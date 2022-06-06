8313655428a8e4d8bbdcfd9ba0641fd3f79cb726
antoni andre <antoniandre.web@gmail.com>
2022-06-06 11:29:50 -0600

    Update the isolated test page.

--- src/views/isolated-test-view.vue
+++ src/views/isolated-test-view.vue
@@ -1,8 +1,9 @@
 <template lang="pug">
 //- This is an isolated test view. Just for testing purpose.
 div
+  w-button.mr2(@click="toggleHorizontal") Horizontal
   w-button(@click="hidePane2 = !hidePane2") toggle pane 2
-  splitpanes.default-theme(style="height: 400px")
+  splitpanes.default-theme(:horizontal="horizontal" style="height: 400px")
     pane(size="85") 1
     pane(size="5" v-if="!hidePane2") 2
     pane(size="10") 3
@@ -13,13 +14,17 @@ import { ref } from 'vue'
 import { Splitpanes, Pane } from '@/components/splitpanes/index'
 
 const components = { Splitpanes, Pane }
-const horizontal = true
-const hidePane2 = ref(false)
+let horizontal = ref(true)
+let hidePane2 = ref(false)
 const panesNumber = 3
 
-// const togglePane2 = () => {
-//   hidePane2 = !hidePane2
-// }
+const togglePane2 = () => {
+  hidePane2.value = !hidePane2.value
+}
+
+const toggleHorizontal = () => {
+  horizontal.value = !horizontal.value
+}
 </script>
 
 <style lang="scss">
