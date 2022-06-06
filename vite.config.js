95ed80b0023edf14407b163a2f5d1e90cb786146
antoni andre <antoniandre.web@gmail.com>
2022-06-06 11:17:46 -0600

    Expose SCSS variables.

--- vite.config.js
+++ vite.config.js
@@ -44,5 +44,12 @@ export default defineConfig({
       '@': resolve(__dirname, '/src')
     }
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "@/scss/_variables.scss";'
+      }
+    }
+  },
   build
 })
