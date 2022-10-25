a4e78138cf33bc5d558f344e9dc0f848b800b48d
antoniandre <antoniandre.web@gmail.com>
2022-10-25 12:14:28 +0200

    Update & cleanup ESLint config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,25 @@
 module.exports = {
-  root: true,
   env: {
+    browser: true,
+    es2021: true,
     node: true
   },
-  'extends': [
-    'plugin:vue/vue3-essential'
+  extends: [
+    'plugin:vue/vue3-essential',
+    'standard'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue'
   ],
   rules: {
     'brace-style': [2, 'stroustrup', { 'allowSingleLine': true }],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
   }
 }
