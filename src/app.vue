4a6f3b88633465e82ced3cb820b00fdd07e648b8
Dominic Hannah <dominic.hannah@education.nsw.gov.au>
2024-07-08 19:27:13 +1000

    switch to composition api in app.vue

--- src/app.vue
+++ src/app.vue
@@ -26,32 +26,30 @@ w-app(block v-scroll="onScroll")
         | View this project on #[a(href="https://github.com/antoniandre/splitpanes" target="_blank") #[w-icon fab fa-github] Github].
 </template>
 
-<script>
-import '@/scss/index.scss'
 
-export default {
-  data: () => ({
-    offsetTop: 0,
-    goTopHidden: true
-  }),
-  methods: {
-    onScroll () {
-      this.offsetTop = window.scrollY || document.documentElement.scrollTop
-      this.goTopHidden = this.offsetTop < 200
-    },
-    scrollToTop () {
-      document.documentElement.scrollTo({ top: 0, behavior: 'smooth' })
-    }
-  },
-  directives: {
-    scroll: {
-      mounted: (el, binding) => {
-        const f = evt => {
-          if (binding.value(evt, el)) window.removeEventListener('scroll', f)
-        }
-        window.addEventListener('scroll', f)
-      }
-    }
-  }
+<script setup lang="ts">
+import '@/scss/index.scss'
+import {ref, DirectiveBinding} from 'vue'
+import type {Ref} from 'vue'
+const offsetTop: Ref<number> = 0
+const goTopHidden: Ref<boolean> = true
+const onScroll = () => {
+  offsetTop.value = window.scrollY || document.documentElement.scrollTop
+  goTopHidden.value = offsetTop.value < 200
 }
+const scrollToTop = () => {
+  document.documentElement.scrollTo({ top: 0, behavior: 'smooth' })
+}
+const scrollDirective = {
+  mounted(el: HTMLElement, binding: DirectiveBinding<(evt: Event, el: HTMLElement) => boolean>) {
+    const f = (evt: Event) => {
+      if (binding.value(evt, el)) {
+        window.removeEventListener('scroll', f);
+      }
+    };
+
+    window.addEventListener('scroll', f);
+  },
+};
+
 </script>
