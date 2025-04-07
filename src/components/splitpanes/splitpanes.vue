eaf152b48018f0a88a230e58e09397804ad4b68d
clemvnt <clemvnt@gmail.com>
2025-04-07 21:44:30 +0200

    fix

--- src/components/splitpanes/splitpanes.vue
+++ src/components/splitpanes/splitpanes.vue
@@ -152,11 +152,14 @@ const onSplitterDblClick = (event, splitterIndex) => {
 }
 
 const onPaneClick = (event, paneId) => {
-  emitEvent('pane-click', {
-    event,
-    index: indexedPanes.value[paneId].index,
-    pane: indexedPanes.value[paneId]
-  })
+  const pane = indexedPanes.value[paneId]
+  if (pane) {
+    emitEvent('pane-click', {
+      event,
+      index: pane.index,
+      pane
+    })
+  }
 }
 
 // Get the cursor position relative to the splitpanes container.
